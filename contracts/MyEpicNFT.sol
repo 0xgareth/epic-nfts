da39a4943dc511f948402424490adcf14b57e45d
gareth-was-here <gareth@garethveale.dev>
2022-01-26 11:14:50 +0000

    inherit from ERC721 contract + add public mint function

--- contracts/MyEpicNFT.sol
+++ contracts/MyEpicNFT.sol
@@ -2,10 +2,33 @@
 
 pragma solidity ^0.8.0;
 
+// import OpenZeppelin contracts
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 import "hardhat/console.sol";
 
-contract MyEpicNFT {
-    constructor() {
+// inherit imported contract
+contract MyEpicNFT is ERC721URIStorage {
+    
+    // magic from OZ to track token ids
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+    
+    // pass name of NFT token + its symbol
+    constructor() ERC721 ("SquareNFT", "Square") {
         console.log("This is my NFT contract. Whoa!");
+    }
+
+    // hit to mint/get NFT
+    function makeAnEpicNFT() public {
+        uint256 newItemId = _tokenIds.current();
+
+        // minft nft to sender via msg.sender
+        _safeMint(msg.sender, newItemId);
+        // set nft data
+        _setTokenURI(newItemId, "https://jsonkeeper.com/b/IJY3");
+        console.log("An NFT w/ ID %s has been minted to %s", newItemId, msg.sender);
+        // increment counter for next NFT
+        _tokenIds.increment();
     }
 }
