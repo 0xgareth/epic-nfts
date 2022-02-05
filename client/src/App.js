bf48be68581469cb297e26b4d4811933546f86fa
gareth-was-here <gareth@garethveale.dev>
2022-02-05 13:47:53 +0000

    capture the token id from contract and give it to users+open sea link

--- client/src/App.js
+++ client/src/App.js
@@ -5,10 +5,11 @@ import { ethers } from "ethers";
 import myEpicNft from './utils/MyEpicNFT.json';
 
 // Constants
-const TWITTER_HANDLE = '_buildspace';
+const TWITTER_HANDLE = 'gareth_xyz';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 const OPENSEA_LINK = '';
 const TOTAL_MINT_COUNT = 50;
+const CONTRACT_ADDRESS = "0x011f8a8d4E90b3897d89d22B76a5eC13AC0f804E";
 
 const App = () => {
 
@@ -31,6 +32,7 @@ const App = () => {
       const account = accounts[0];
       console.log("Found an authorized account:", account);
       setCurrentAccount(account);
+      setupEventListener();
     } else {
       console.log("No authorized account found :( !!");
     }
@@ -51,13 +53,40 @@ const App = () => {
       // log public address of connected account
       console.log("Connected", accounts[0]);
       setCurrentAccount(accounts[0]);
+      setupEventListener();
     } catch (error) {
       console.log(error);
     }
   };
 
+  const setupEventListener = async () => {
+    // Most of this looks the same as our function askContractToMintNft
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, myEpicNft.abi, signer);
+
+        // THIS IS THE MAGIC SAUCE DAWG
+        // This will essentially "capture" our event when our contract throws it - obtaining token id
+        connectedContract.on("NewEpicNFTMinted", (from, tokenId) => {
+          console.log(from, tokenId.toNumber())
+          alert(`Hey there! We've minted your NFT and sent it to your wallet. It may be blank right now. It can take a max of 10 min to show up on OpenSea. Here's the link: https://testnets.opensea.io/assets/${CONTRACT_ADDRESS}/${tokenId.toNumber()}`)
+        });
+
+        console.log("Setup event listener!")
+
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   const askContractToMintNFT = async () => {
-    const CONTRACT_ADDRESS = "0x011f8a8d4E90b3897d89d22B76a5eC13AC0f804E";
 
     try {
       const { ethereum } = window;
@@ -117,7 +146,7 @@ const App = () => {
             href={TWITTER_LINK}
             target="_blank"
             rel="noreferrer"
-          >{`built on @${TWITTER_HANDLE}`}</a>
+          >{`built by @${TWITTER_HANDLE}`}</a>
         </div>
       </div>
     </div>
